f9a496cb42363ce02d5a36d28904a469ec46be00
Zephyr Pellerin <zephyr.pellerin@gmail.com>
2012-02-14 10:27:59 -0800

    More cleanup in membership to check if nodes respond to sync_event

--- src/artifact_membership.erl
+++ src/artifact_membership.erl
@@ -1,14 +1,20 @@
-% Licensed under the Apache License, Version 2.0 (the "License"); you may not
-% use this file except in compliance with the License.  You may obtain a copy of
-% the License at
 %
-%   http://www.apache.org/licenses/LICENSE-2.0
+% Artifact Data Store
 %
-% Unless required by applicable law or agreed to in writing, software
-% distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-% WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
-% License for the specific language governing permissions and limitations under
-% the License.
+% Copyright (c) 2012 Zephyr Pellerin
+% All rights reserved.
+%
+% Redistribution and use in source and binary forms are permitted
+% provided that the above copyright notice and this paragraph are
+% duplicated in all such forms and that any documentation,
+% advertising materials, and other materials related to such
+% distribution and use acknowledge that the software was developed
+% by Zephyr Pellerin.  My name cannot be used in endorsement of any product
+% derived from this software. Buy me a beer sometime if you liked this
+% THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+% IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+% WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. 
+%
 
 -module(artifact_membership).
 -behaviour(gen_fsm).
@@ -22,78 +28,78 @@
 
 -include("artifact.hrl").
 
+-record(state, {node, interval}).
+
 -define(SERVER, ?MODULE).
 
 start_link() ->
     gen_fsm:start_link({local, ?SERVER}, ?MODULE, [], _Opts = []).
     
 init(_Args) ->
-    {ok, ready, [], ?TIMER}.
+    [LocalNode, Interval] = artifact_config:get([node, sync_interval]),
+    {ok, ready, #state{
+           node     = LocalNode,
+           interval = Interval
+          }, Interval}.
 
 terminate(_Reason, _StateName, _StateData) ->
     ok.
 
-ping_nodes([], AvailableNodes, DownNodes) ->
-    {AvailableNodes, DownNodes};
-ping_nodes([Node|Nodes], AvailableNodes, DownNodes) ->
-    case artifact_rpc:node_info(Node) of
-        {node_info, Node2, Info} ->
-            ping_nodes(Nodes, [{Node2, Info}|AvailableNodes], DownNodes);
-        {error, Reason} ->
-            ?warning(io_lib:format("ping_nodes/3: ~p", [{error, Reason}])),
-            ping_nodes(Nodes, AvailableNodes, [Node|DownNodes])
-    end.
+do_check_node({IpAddr, Port}, State) ->
+    {AvailableNodes, DownNodes} = retrieve_node_list({IpAddr, Port}, State),
+    {ok, ReplacedBuckets} = artifact_hash:update_nodes(AvailableNodes, DownNodes),
+    sync_buckets(ReplacedBuckets).
 
-retrieve_node_list(Node) ->
-    case artifact_rpc:node_list(Node) of
-        {node_list, RemoteNodeList} ->
-            {node_list, LocalNodeList} = artifact_hash:node_list(),
+retrieve_node_list(Node, State) ->
+    case artifact_rpc:node_list(Node, State#state.node) of
+        {ok, RemoteNodeList} ->
+            {ok, LocalNodeList} = artifact_hash:node_list(),
             NewNodes = RemoteNodeList -- LocalNodeList,
             OldNodes = LocalNodeList  -- RemoteNodeList,
             Nodes = NewNodes ++ OldNodes,
-            LocalNode = artifact_config:get(node),
-            ping_nodes(Nodes -- [LocalNode], [], []);
+            ping_nodes(Nodes -- [State#state.node], [], [], State);
         {error, Reason} ->
             ?warning(io_lib:format("retrieve_node_list/1: ~p", [{error, Reason}])),
             {[], [Node]}
     end.
 
-sync_buckets([], _LocalNode) ->
+ping_nodes([], AvailableNodes, DownNodes, _State) ->
+    {AvailableNodes, DownNodes};
+ping_nodes([Node|Nodes], AvailableNodes, DownNodes, State) ->
+    case artifact_rpc:node_info(Node, State#state.node) of
+        {ok, Node2, Info} ->
+            ping_nodes(Nodes, [{Node2, Info}|AvailableNodes], DownNodes, State);
+        {error, Reason} ->
+            ?warning(io_lib:format("ping_nodes/3: ~p", [{error, Reason}])),
+            ping_nodes(Nodes, AvailableNodes, [Node|DownNodes], State)
+    end.
+
+sync_buckets([]) ->
     ok;
-sync_buckets([{Bucket, NewReplica, OldReplica}|ReplacedBuckets], LocalNode) ->
+sync_buckets([{Bucket, NewReplica, OldReplica}|ReplacedBuckets]) ->
     case {NewReplica, OldReplica} of
         {NewReplica, undefined } -> artifact_sync:update_bucket(Bucket);
         {undefined,  OldReplica} -> artifact_sync:delete_bucket(Bucket);
         _                        -> nop
     end,
-    sync_buckets(ReplacedBuckets, LocalNode).
-
-sync_buckets(ReplacedBuckets) ->
-    LocalNode = artifact_config:get(node),
-    sync_buckets(ReplacedBuckets, LocalNode).
-
-do_check_node({Address, Port}) ->
-    {AvailableNodes, DownNodes} = retrieve_node_list({Address, Port}),
-    {replaced_buckets, ReplacedBuckets} =
-        artifact_hash:update_nodes(AvailableNodes, DownNodes),
     sync_buckets(ReplacedBuckets).
 
 ready({check_node, Node}, State) ->
-    do_check_node(Node),
-    {next_state, ready, State, ?TIMER};
+    do_check_node(Node, State),
+    {next_state, ready, State, State#state.interval};
 ready(timeout, State) ->
     case artifact_hash:choose_node_randomly() of
-        {node, Node} -> do_check_node(Node);
+        {node, Node} -> do_check_node(Node, State);
         _            -> nop
     end,
-    {next_state, ready, State, ?TIMER}.
+    {next_state, ready, State, State#state.interval}.
 
 handle_event(stop, _StateName, StateData) ->
     {stop, normal, StateData}.
 handle_sync_event(_Event, _From, _StateName, StateData) ->
-    {next_state, ready, StateData, ?TIMEOUT}.
+    {next_state, ready, StateData, _Interval = 1000}.
 handle_info(_Info, _StateName, StateData) ->
-    {next_state, ready, StateData, ?TIMEOUT}.
+    {next_state, ready, StateData, _Interval = 1000}.
 code_change(_OldVsn, _StateName, StateData, _Extra) ->
     {ok, ready, StateData}.
 
