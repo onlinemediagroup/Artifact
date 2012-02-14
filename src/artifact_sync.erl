03dd09f27d053b640eab9f9344c9ad323bb78a00
Zephyr Pellerin <zephyr.pellerin@gmail.com>
2012-02-14 11:07:05 -0800

    Merging in rewrite for MixRank

--- src/artifact_sync.erl
+++ src/artifact_sync.erl
@@ -15,6 +15,8 @@
 % IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
 % WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. 
 %
+
+
 -module(artifact_sync).
 -behaviour(gen_fsm).
 
@@ -27,80 +29,88 @@
 
 -include("artifact.hrl").
 
+-record(state, {node, interval}).
+
 -define(SERVER, ?MODULE).
 
 start_link() ->
     gen_fsm:start_link({local, ?SERVER}, ?MODULE, [], _Opts = []).
     
 init(_Args) ->
-    {ok, ready, [], ?TIMER}.
+    [LocalNode, Interval] = artifact_config:get([node, sync_interval]),
+    {ok, ready, #state{
+           node     = LocalNode,
+           interval = Interval
+          }, Interval}.
 
 terminate(_Reason, _StateName, _StateData) ->
     ok.
 
-retrieve_data(_Node, []) ->
+do_update_bucket(Bucket, State) when is_record(State, state) ->
+    {ok, Nodes} = artifact_hash:find_nodes(Bucket),
+    do_update_bucket(Bucket, Nodes -- [State#state.node], State).
+
+do_update_bucket(_Bucket, [], _State) ->
+    {error, enodata};
+do_update_bucket(Bucket, [Node|Rest], State) ->
+    case artifact_rpc:list(Node, State#state.node, Bucket) of
+        {ok, KeyList} ->
+            retrieve_data(Node, KeyList, State);
+        {error, Reason} ->
+            ?warning(io_lib:format("do_update_bucket/2: ~p", [{error, Reason}])),
+            do_update_bucket(Bucket, Rest, State)
+    end.
+
+retrieve_data(_Node, [], _State) ->
     ok;
-retrieve_data(Node, [Metadata|Rest]) ->
+retrieve_data(Node, [Metadata|Rest], State) ->
     case artifact_store:get(Metadata) of
-        Data when is_record(Data, data) ->
-            retrieve_data(Node, Rest);
+        DataList when is_list(DataList) ->
+            retrieve_data(Node, Rest, State);
         undefined ->
-            case artifact_rpc:get(Node, Metadata) of
-                Data when is_record(Data, data) ->
-                    artifact_store:put(Data),
-                    retrieve_data(Node, Rest);
+            case artifact_rpc:get(Node, State#state.node, Metadata) of
+                RemoteDataList when is_list(RemoteDataList) ->
+                    lists:map(fun(RemoteData) ->
+                                      artifact_store:put(RemoteData) end,
+                              RemoteDataList),
+                    retrieve_data(Node, Rest, State);
                 undefined ->
-                    retrieve_data(Node, Rest);
+                    retrieve_data(Node, Rest, State);
                 {error, Reason} ->
                     ?warning(io_lib:format("retrieve_data/2: ~p", [{error, Reason}])),
                     {error, Reason}
             end
     end.
 
-do_update_bucket(_Bucket, []) ->
-    {error, enodata};
-do_update_bucket(Bucket, [Node|Rest]) ->
-    case artifact_rpc:list(Node, Bucket) of
-        {list_of_data, ListOfData} ->
-            retrieve_data(Node, ListOfData);
-        {error, Reason} ->
-            ?warning(io_lib:format("do_update_bucket/2: ~p", [{error, Reason}])),
-            do_update_bucket(Bucket, Rest)
-    end.
-
-do_update_bucket(Bucket) ->
-    {nodes, Nodes} = artifact_hash:find_nodes(Bucket),
-    LocalNode = artifact_config:get(node),
-    do_update_bucket(Bucket, Nodes -- [LocalNode]).
-
 do_delete_bucket([]) ->
+    % todo - fix this.
     ok;
 do_delete_bucket([Metadata|Rest]) ->
     artifact_store:delete(Metadata),
     do_delete_bucket(Rest);
 do_delete_bucket(Bucket) ->
-    {list_of_data, ListOfData} = artifact_store:list(Bucket),
-    do_delete_bucket(ListOfData).
+    {ok, KeyList} = artifact_store:list(Bucket),
+    do_delete_bucket(KeyList).
 
 ready({update_bucket, Bucket}, State) ->
-    do_update_bucket(Bucket),
-    {next_state, ready, State, ?TIMER};
+    do_update_bucket(Bucket, State),
+    {next_state, ready, State, State#state.interval};
 ready({delete_bucket, Bucket}, State) ->
     do_delete_bucket(Bucket),
-    {next_state, ready, State, ?TIMER};
+    {next_state, ready, State, State#state.interval};
 ready(timeout, State) ->
     case artifact_hash:choose_bucket_randomly() of
-        {bucket, Bucket} -> do_update_bucket(Bucket);
-        _                -> nop
+        {ok, Bucket} -> do_update_bucket(Bucket, State);
+        _            -> nop
     end,
-    {next_state, ready, State, ?TIMER}.
+    {next_state, ready, State, State#state.interval}.
 
 handle_event(stop, _StateName, StateData) ->
     {stop, normal, StateData}.
 handle_sync_event(_Event, _From, _StateName, StateData) ->
-    {next_state, wait, StateData, ?TIMEOUT}.
+    {next_state, wait, StateData, _Interval = 1000}.
 handle_info(_Info, _StateName, StateData) ->
-    {next_state, ready, StateData, ?TIMER}.
+    {next_state, ready, StateData, _Interval = 1000}.
 code_change(_OldVsn, _StateName, StateData, _Extra) ->
     {ok, ready, StateData}.
 
